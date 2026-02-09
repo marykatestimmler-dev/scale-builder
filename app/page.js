c1233f03a135365577afead628ac5ef44825ad50
Mary Kate Stimmler <mary.kate.stimmler@gmail.com>
2026-02-09 05:38:53 -0800

    Remove HEXACO-60 (not truly open source)

--- app/page.js
+++ app/page.js
@@ -230,7 +230,7 @@ export default function Page() {
             </p>
 
             {/* Badge */}
-            <div className="badge-pill mb-8">85 Validated Scales</div>
+            <div className="badge-pill mb-8">84 Validated Scales</div>
             <p className="text-sm text-gray-400 mt-2 mb-8 italic">Can&apos;t find your favorite scale? This site aims to use only open source scales.</p>
 
             {/* How It Works */}
